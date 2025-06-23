0ff616b5c583a4b16f26bd480b087be6955cc9c4
tinnguyen <tinn1855@gmail.com>
2025-06-23 12:00:41 +0700

    fix sidebar

--- src/components/molecules/sidebar/index.tsx
+++ src/components/molecules/sidebar/index.tsx
@@ -1,16 +1,43 @@
-import { Calendar, Home, Inbox, Search, Settings } from 'lucide-react';
+import {
+  Calendar,
+  Calendar1,
+  ChartNoAxesCombined,
+  ChevronDown,
+  ChevronUp,
+  DollarSign,
+  FileChartColumnIncreasing,
+  Hand,
+  Home,
+  Inbox,
+  Search,
+  Settings,
+  User,
+  User2,
+  Users,
+} from 'lucide-react';
 
 import {
   Sidebar,
   SidebarContent,
+  SidebarFooter,
   SidebarGroup,
   SidebarGroupContent,
   SidebarGroupLabel,
+  SidebarHeader,
   SidebarMenu,
   SidebarMenuButton,
   SidebarMenuItem,
   SidebarTrigger,
+  useSidebar,
 } from '@/components/ui/sidebar';
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
+import { useState } from 'react';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
 
 // Menu items.
 const items = [
@@ -20,30 +47,62 @@ const items = [
     icon: Home,
   },
   {
-    title: 'Inbox',
+    title: 'Quản lý nhân sự',
+    url: '#',
+    icon: Users,
+  },
+  {
+    title: 'Quản lý công việc',
     url: '#',
     icon: Inbox,
   },
   {
-    title: 'Calendar',
+    title: 'Quản lý phòng ban',
     url: '#',
-    icon: Calendar,
+    icon: Inbox,
   },
   {
-    title: 'Search',
+    title: 'Quản lý lương',
     url: '#',
-    icon: Search,
+    icon: DollarSign,
   },
   {
-    title: 'Settings',
+    title: 'Chấm công',
     url: '#',
-    icon: Settings,
+    icon: Hand,
+  },
+  {
+    title: 'Nghỉ phép & Xin phép',
+    url: '#',
+    icon: Calendar1,
+  },
+  {
+    title: 'Báo cáo, thống kê',
+    url: '#',
+    icon: FileChartColumnIncreasing,
   },
 ];
 
 export function AppSidebar() {
+  const { open } = useSidebar();
+
   return (
     <Sidebar collapsible="icon">
+      <SidebarHeader>
+        <SidebarMenu>
+          <SidebarMenuItem className="flex items-center justify-between">
+            <div className={`${open ? 'flex gap-2 items-center' : 'hidden'}`}>
+              <Avatar>
+                <AvatarImage src="https://github.com/shadcn.png" />
+                <AvatarFallback>CN</AvatarFallback>
+              </Avatar>
+              <h3 className="font-medium">Admin</h3>
+            </div>
+            <SidebarTrigger />
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarHeader>
+
       <SidebarContent>
         <SidebarGroup>
           <SidebarGroupLabel>Application</SidebarGroupLabel>
@@ -63,7 +122,28 @@ export function AppSidebar() {
           </SidebarGroupContent>
         </SidebarGroup>
       </SidebarContent>
-      <SidebarTrigger />
+      <SidebarFooter>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <SidebarMenuButton>
+                  <User2 /> tinn1855@gmail.com
+                  <ChevronUp className="ml-auto" />
+                </SidebarMenuButton>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent
+                side="top"
+                className="w-[--radix-popper-anchor-width]"
+              >
+                <DropdownMenuItem>
+                  <span>Log out</span>
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarFooter>
     </Sidebar>
   );
 }
